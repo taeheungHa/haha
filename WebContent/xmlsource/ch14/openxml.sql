51281f54506bbb8b4c61c2dbba7ca93a2f470f6a
taeheungHa <htloveyj0525@naver.com>
2013-12-10 15:12:56 +0900

    ??

--- WebContent/xmlsource/ch14/openxml.sql
+++
@@ -1,8 +0,0 @@
-CREATE PROCEDURE insertBook @doc VARCHAR(2000)
-AS
-DECLARE @idoc int
-EXEC sp_xml_preparedocument @idoc OUTPUT, @doc
-INSERT INTO book(bid, kind, title, publisher, price)
-SELECT * FROM OPENXML(@idoc, 'booklist/book', 3) WITH book
-EXEC sp_xml_removedocument @idoc
-
